75dd9d8fd6cd1f0dd6e31ca4bae1d7704c769eb1
karkgdry <2276089452@qq.com>
2023-10-17 12:03:03 +0800

    element-plus自动按需导入配置

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+//element-plus 按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     //实际的路径转换 @->src
