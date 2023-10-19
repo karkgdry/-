de76bf95c4cca479102018f972d1864fbe19e7cf
karkgdry <2276089452@qq.com>
2023-10-19 20:57:24 +0800

    layout渲染一级导航列表

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-//element-plus 按需导入
+import vue from '@vitejs/plugin-vue'
+
+// elementPlus按需导入
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // ...
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver({importStyle:"sass"})],
+      resolvers: [
+        // 1. 配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({ importStyle: "sass" }),
+      ],
     }),
   ],
   resolve: {
-    //实际的路径转换 @->src
+    // 实际的路径转换  @  -> src
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
@@ -27,7 +32,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        // 自动导入定制化样式文件进行样式覆盖
+        // 2. 自动导入定制化样式文件进行样式覆盖
         additionalData: `
           @use "@/styles/element/index.scss" as *;
           @use "@/styles/var.scss" as *;
