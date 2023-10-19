6d72485d1b716fd708faf5f1e328c9d0019ed392
karkgdry <2276089452@qq.com>
2023-10-19 21:19:43 +0800

    吸顶导航交互实现

--- src/main.js
+++ src/main.js
@@ -7,11 +7,7 @@ import router from './router'
 //引入初始化样式文件
 import '@/styles/common.scss'
 
-//测试接口函数
-import { getCategory } from './apis/testApi'
-getCategory().then(res => {
-    console.log(res);
-})
+
 const app = createApp(App)
 
 app.use(createPinia())
