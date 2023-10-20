89cdee0d145c1c4da46c5689a76a5f4999b853c0
karkgdry <2276089452@qq.com>
2023-10-20 20:48:39 +0800

    goods-item组件的封装

--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -4,7 +4,6 @@ import { ref,onMounted } from 'vue'
 const bannerList = ref([])
 const getBanner = async () => {
     const res = await getBannerAPI()
-    console.log(res);
     bannerList.value=res.result
 }
 onMounted(() => getBanner())
