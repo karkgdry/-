7986339416ccb1b4b619d428d074e663d5580f4f
karkgdry <2276089452@qq.com>
2023-10-26 21:45:56 +0800

    详情-基础数据渲染

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
-
+import Detail from '@/views/Detail/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   //配置path和component对应关系的位置
@@ -18,12 +18,16 @@ const router = createRouter({
           component:Home
         },
         {
-          path: '/category/:id',
+          path: 'category/:id',
           component:Category
         },
         {
-          path: '/category/sub/:id',
+          path: 'category/sub/:id',
           component:SubCategory
+        },
+        {
+          path: 'detail/:id',
+          component:Detail
         }
       ]
     },
