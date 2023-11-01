4e6d05b2170c11ebfcad3adc817a04817f82e69a
karkgdry <2276089452@qq.com>
2023-11-01 16:29:55 +0800

    完成登录表单的校验功能

--- src/main.js
+++ src/main.js
@@ -9,12 +9,13 @@ import '@/styles/common.scss'
 
 //引入懒加载指令插件并注册
 import { lazyPlugin } from './directives'
-
+import { componentPlugin } from '@/components'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
+app.use(componentPlugin)
 app.mount('#app')
 
 
